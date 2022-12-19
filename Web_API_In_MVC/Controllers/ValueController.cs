e4d70d9523f1fa8313c429ef52f493e83c1ce032
Anurag8112 <katiyaranu2003@gmail.com>
2022-12-19 11:56:04 +0530

    Add project files.

---
+++ Web_API_In_MVC/Controllers/ValueController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_API_In_MVC.Controllers
+{
+    public class ValueController : ApiController
+    {
+        [Route("api/get")]
+        public IHttpActionResult Get()
+        {
+            return Ok(1);
+        }
+    }
+}
---
+++ Web_API_In_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_API_In_MVC.MvcApplication" Language="C#" %>
